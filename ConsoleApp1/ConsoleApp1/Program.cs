eacc944e7a74440a0db107c23954e1d37145e1c0
dchaudhari9088 <dchaudhari9088@gmail.com>
2020-05-06 19:14:14 +0530

    added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Uri address2 = new Uri("https://intra.ema.kpmg.com/sites/ZA/HR/Lists/My%20Performance%20Development%20%20MyPD%20%20%20promoted%20links/Tiles.aspx#InplviewHash75cbd3f1-b3bf-45a4-a370-a2059dbc3195=ShowInGrid%3DTrue");
+
+            Console.WriteLine("Hello World!" + address2.AbsolutePath);
         }
     }
 }
